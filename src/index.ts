e5e22ca7921af7e39ac0f03f66710b71597e84f3
you <nettanel1999@gmail.com>
2024-09-30 10:34:49 +0300

    index.js

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express'
 import cookieParser from 'cookie-parser'
+import 'dotenv/config'
 
 const app: Express = express()
 
@@ -7,3 +8,7 @@ const PORT: number = 1234
 
 app.use(express.json())
 app.use(cookieParser())
+
+app.listen(process.env.PORT,() =>{
+    console.log(`you are in at port ${process.env.PORT}`)
+})
