fbdc1ff45177ea889311408345dc4832bd6498d5
you <nettanel1999@gmail.com>
2024-09-30 11:11:18 +0300

    adding using in index and structure of controllers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express, { Express } from 'express'
 import cookieParser from 'cookie-parser'
 import 'dotenv/config'
+import authCtrl from './controllers/authController'
+import userCtrl from './controllers/userController'
+import todosCtrl from './controllers/todosController'
+
 
 const app: Express = express()
 
@@ -8,6 +12,10 @@ const PORT: number = 1234
 
 app.use(express.json())
 app.use(cookieParser())
+
+app.use('/auth',authCtrl)
+app.use('/users',userCtrl)
+app.use('/todos',todosCtrl)
 
 app.listen(process.env.PORT,() =>{
     console.log(`you are in at port ${process.env.PORT}`)
